61b81b80a0d2bfebce9ad3ca8b885438404390f3
Vinayak Gadag <vsharanappagadag@deloitte.com>
2020-05-26 11:33:30 +0530

    Update

--- aws-cicd/codedeploy/commands.sh
+++ aws-cicd/codedeploy/commands.sh
@@ -5,7 +5,7 @@ sudo yum update
 sudo yum install ruby
 
 # Download the agent (replace the region)
-wget https://aws-codedeploy-eu-west-3.s3.eu-west-3.amazonaws.com/latest/install
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent status
